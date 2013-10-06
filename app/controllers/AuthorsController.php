2745919cbd90ab156ea3f4c454117dfdb30d31c8
himajin315 <hiamjin315@gmail.com>
2013-10-07 00:22:59 +0900

    Laravel Tutorial Part4

--- app/controllers/AuthorsController.php
+++ app/controllers/AuthorsController.php
@@ -1,16 +1,17 @@
 <?php
 
 class AuthorsController extends BaseController {
-
  
   public $restful = true;
+  public $layout = 'layouts.default';
 
   public function getIndex()
   {
     $view = View::make('authors.index', array('name'=>'Andrew Perkins'))
       ->with('age','28');
-    $view->location='California';
-    $view['specialty']= 'PHP';
-    return $view;
+    $view->location = 'California';
+    $view['specialty'] = 'PHP';
+    $this->layout->title = 'Authors and Books from Controller';
+    $this->layout->content = $view;
   }
 }
